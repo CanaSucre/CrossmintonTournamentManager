cf3eb47c4abc4641fb7ca558ad957a1160c20dcf
Cana_sucre <88255918+CanaSucre@users.noreply.github.com>
2026-01-15 11:28:48 +0100

    ADD - Filter + refactor tournament

--- web/public/js/tournament.js
+++ web/public/js/tournament.js
@@ -1,23 +1,80 @@
 // Récupère le l'ID du tournoi à partir de l'URL : /tournament/:id
 const tournamentId = window.location.pathname.split('/').pop();
-
 const socket = io(`/tournament/${tournamentId}`);
 
+let currentFilterStatus = "all";
+let currentFilterCategory = "all";
+let currentSort = "num_match";
 
-socket.on("load", (data) => {
-    loadTournamentDatas(data);
+let tournamentDatas = {};
+
+socket.once("load", (data) => {
+    tournamentDatas = data;
+
+    loadTournamentDatas();
+});
+
+
+document.getElementById("matchFilter_status").addEventListener("change", (event) => {
+    currentFilterStatus = event.target.value;
+    applyFiltersAndSorting();
+});
+
+document.getElementById("matchFilter_category").addEventListener("change", (event) => {
+    currentFilterCategory = event.target.value;
+    applyFiltersAndSorting();
+});
+
+document.getElementById("matchSort").addEventListener("change", (event) => {
+    currentSort = event.target.value;
+    applyFiltersAndSorting();
 });
 
 
 /**
+ * Applique les filtres et le tri aux données du tournoi
+ */
+function applyFiltersAndSorting() {
+    let categorieList = getCategoryList();
+    let matchList = tournamentDatas.matchs;
+
+    // Filtrer par statut
+    if (currentFilterStatus !== "all") {
+        matchList = matchList.filter(match => match.statut === currentFilterStatus);
+    }
+
+    // Filtrer par catégorie
+    if (currentFilterCategory !== "all") {
+        matchList = matchList.filter(match => categorieList.indexOf(match.category) == currentFilterCategory);
+    }
+
+    // Trier les matchs
+    switch (currentSort) {
+        case "num_match":
+            matchList.sort((a, b) => a.idMatch - b.idMatch);
+            break;
+        
+        case "categorie":
+            matchList.sort((a, b) => {
+                if (a.category < b.category) return -1;
+                if (a.category > b.category) return 1;
+                return 0;
+            });
+            break;
+    }
+
+
+    loadMatchesList(matchList);
+}
+
+/**
  * Compte le nombre de matchs joués dans une liste de matchs
- * @param {Array<Object>} matchs 
  * @returns {Integer}
  */
-function countPlayedMatches(matchs) {
+function countPlayedMatches() {
     let count = 0;
 
-    matchs.forEach(match => {
+    tournamentDatas.matchs.forEach(match => {
         if (match.statut === "completed") {
             count++;
         }
@@ -29,23 +86,33 @@ function countPlayedMatches(matchs) {
 
 /**
  * Charge les données du tournoi dans la page
- * @param {Object} datas Données du tournoi
  */
-function loadTournamentDatas(datas) {
+function loadTournamentDatas() {
     let title = document.getElementById("tournamentName");
-    title.innerText = datas.tournamentInfos.nomTournoi;
+    title.innerText = tournamentDatas.tournamentInfos.nomTournoi;
 
-    loadActionBarButtons(datas);
-    loadStatBar(datas);
-    loadMatchesList(datas);
+    loadActionBarButtons(tournamentDatas);
+    loadStatBar(tournamentDatas);
+    loadMatchesList(tournamentDatas.matchs);
+
+
+     // Remplir les options de filtre par catégorie
+    let categoryFilterSelect = document.getElementById("matchFilter_category");
+    let categories = getCategoryList();
+
+    categories.forEach((category, index) => {
+        let option = document.createElement("option");
+        option.value = index;
+        option.text = category;
+        categoryFilterSelect.appendChild(option);
+    });
 }
 
 
 /**
  * Charge les boutons de la barre d'action en fonction de l'état du tournoi
- * @param {Object} datas Données du tournoi
  */
-function loadActionBarButtons(datas) {
+function loadActionBarButtons() {
     let actionBarBtn = document.getElementById("actionBarBtn");
     actionBarBtn.innerHTML = "";
 
@@ -53,14 +120,14 @@ function loadActionBarButtons(datas) {
     actionBarBtn.innerHTML += `<button type="button" class="btn btn-primary col-3"><i class="bi bi-upload"></i> Charger matchs</button>`
 
     // Le bouton est activé que s'il n'y a pas de live en cours sur un autre tournoi et si le tournoi est en cours
-    let buttonDisabled = datas.tournamentInfos.status !== "Ongoing" || datas.liveEnabled ? "disabled": "";
-    if (datas.isLive) {
+    let buttonDisabled = tournamentDatas.tournamentInfos.status !== "Ongoing" || tournamentDatas.liveEnabled ? "disabled": "";
+    if (tournamentDatas.isLive) {
         actionBarBtn.innerHTML += `<button type="button" class="btn btn-danger col-3"><i class="bi bi-wifi"></i> Stop LiveScore</button>`;
     } else {
         actionBarBtn.innerHTML += `<button type="button" class="btn btn-success col-3" ${buttonDisabled}><i class="bi bi-wifi"></i> Start LiveScore</button>`;
     }
 
-    switch (datas.tournamentInfos.status) {
+    switch (tournamentDatas.tournamentInfos.status) {
         case "Upcoming": {
             actionBarBtn.innerHTML += `<button type="button" class="btn btn-success col-2"><i class="bi bi-play-fill"></i> Démarrer</button>`;
             break;
@@ -83,13 +150,11 @@ function loadActionBarButtons(datas) {
 
 /**
  * Retourne la liste des joueurs uniques du tournoi
- * @param {Object} datas Données du tournoi
- * @returns 
+ * @returns {Array}
  */
-function getPlayerList(datas) {
-    let playersOne = datas.matchs.map(match => match.joueur1);
-    let playersTwo = datas.matchs.map(match => match.joueur2);
-
+function getPlayerList() {
+    let playersOne = tournamentDatas.matchs.map(match => match.joueur1);
+    let playersTwo = tournamentDatas.matchs.map(match => match.joueur2);
     let allPlayers = playersOne.concat(playersTwo);
 
     let uniquePlayers = Array.from(new Set(allPlayers));
@@ -98,24 +163,33 @@ function getPlayerList(datas) {
 }
 
 /**
+ * Retourne la liste des catégories uniques du tournoi
+ * @returns {Array}
+ */
+function getCategoryList() {
+    let categories = new Set(tournamentDatas.matchs.map(match => match.category));
+    return Array.from(categories);
+}
+
+/**
  * Charge la barre de statistiques du tournoi
  * @param {Object} datas Données du tournoi
  */
-function loadStatBar(datas) {
+function loadStatBar() {
     let statBarContent = document.getElementById("statBarContent");
-    let totalMatches = datas.matchs.length;
-    let playedMatches = countPlayedMatches(datas.matchs);
+    let totalMatches = tournamentDatas.matchs.length;
+    let playedMatches = countPlayedMatches(tournamentDatas.matchs);
 
-    let categories = new Set(datas.matchs.map(match => match.category));
+    let categories = new Set(tournamentDatas.matchs.map(match => match.category));
     let totalCategories = categories.size;
 
-    let amountPlayers = getPlayerList(datas).length;
+    let amountPlayers = getPlayerList().length;
 
     statBarContent.innerHTML = "";
 
     statBarContent.innerHTML = `
         <div class="statItem text-center p-2">
-            <h3>${datas.tournamentInfos.nombreTerrains}</h3>
+            <h3>${tournamentDatas.tournamentInfos.nombreTerrains}</h3>
             <p class="m-0 p-0">terrains</p>
         </div>
         <div class="statItem text-center p-2">
@@ -137,25 +211,48 @@ function loadStatBar(datas) {
  * Affiche la liste des matchs du tournoi
  * @param {Object} datas Données du tournoi
  */
-function loadMatchesList(datas) {
+function loadMatchesList(matchArray) {
     let matchesList = document.getElementById("matchesListContent");
-
-    let categories = new Set(datas.matchs.map(match => match.category));
+    matchesList.innerHTML = "";
     
     let categoriesColor = {};
-    Array.from(categories).forEach((category, index) => categoriesColor[category] = index);
+   getCategoryList().forEach((category, index) => categoriesColor[category] = index);
 
 
-    datas.matchs.forEach(match => {
+    matchArray.forEach(match => {
         matchesList.innerHTML += `
-            <p class="matchEntrie colorMatch-${categoriesColor[match.category]} m-0 d-flex justify-content-between">
-                <span>
-                    <i class="bi bi-circle-fill ${getColorByStatus_match(match.statut)}"></i>
-                    N°${match.idMatch} | ${match.category} - ${match.round} | ${match.joueur1} - ${match.joueur2}
-                </span>
+            <div class="matchEntrie m-0">
+                <div class="d-flex justify-content-between colorMatch-${categoriesColor[match.category]}">
+                    <span>
+                        <i class="bi bi-circle-fill ${getColorByStatus_match(match.statut)}"></i>
+                        N°${match.idMatch} | ${match.category} - ${match.round} | ${match.joueur1} - ${match.joueur2}
+                    </span>
+                    ${match.statut === "completed" ? `<span class="px-2"><i class="bi bi-trophy-fill"></i> ${match.winner}</span>`:``}
+                </div>
 
-                ${match.statut === "completed" ? `<span class="px-2"><i class="bi bi-trophy-fill"></i> ${match.winner}</span>`:``}
-            </p>
+                ${match.statut === "in_progress"  || match.statut === "completed" ? ` 
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>Set 1</th>
+                        <th>Set 2</th>
+                        <th>Set 3</th>
+                    </tr>
+                    <tr>
+                        <td>${match.winner === match.joueur1 ? `<i class="bi bi-trophy-fill"></i> ` : ""}${match.joueur1}</td>
+                        <td>${match.set1Joueur1 ? match.set1Joueur1 : ''}</td>
+                        <td>${match.set2Joueur1 ? match.set2Joueur1 : ''}</td>
+                        <td>${match.set3Joueur1 ? match.set3Joueur1 : ''}</td>
+                    </tr>
+                    <tr>
+                        <td>${match.winner === match.joueur2 ? `<i class="bi bi-trophy-fill"></i> ` : ""}${match.joueur2}</td>
+                        <td>${match.set1Joueur2 ? match.set1Joueur2 : ''}</td>
+                        <td>${match.set2Joueur2 ? match.set2Joueur2 : ''}</td>
+                        <td>${match.set3Joueur2 ? match.set3Joueur2 : ''}</td>
+                    </tr>
+                </table>
+                `: ""}
+            </div>
 
         `;
     });
